c69080c21d45ecab2b710b8f639fa5affad479ca
Ian Zhang <izhang@redhat.com>
2021-10-28 09:21:10 -0400

    some small tweaks
    Signed-off-by: Ian Zhang <izhang@redhat.com>

--- pgo/setup.sh
+++ pgo/setup.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 # install the pgo operator to postgres-operator
-kubectl apply -f ./install
+kubectl apply -k ./install
 
 # set up hoh database hoh
-kubectl apply -f ./high-availability
+kubectl apply -k ./high-availability
 
 PG_NAMESPACE="hoh-postgres"
 DB_NAME="hoh"
 HOH_PGBOUNCER="pgbouncer"
 PGBOUNCER_SERVICE="hoh-pgbouncer"
+
 PG_CLUSTER_USER_SECRET_NAME="$DB_NAME-pguser-$DB_NAME"
+
 PGPASSWORD="$(kubectl get secrets -n "${PG_NAMESPACE}" "${PG_CLUSTER_USER_SECRET_NAME}" -o go-template='{{.data.password | base64decode}}')"
 PGUSER="$(kubectl get secrets -n "${PG_NAMESPACE}" "${PG_CLUSTER_USER_SECRET_NAME}" -o go-template='{{.data.user | base64decode}}')"
 PG_URI="$(kubectl get secrets -n "${PG_NAMESPACE}" "${PG_CLUSTER_USER_SECRET_NAME}" -o go-template='{{.data.uri | base64decode}}')"
@@ -19,9 +21,9 @@ kubectl expose service $PGBOUNCER_SERVICE -n $PG_NAMESPACE --port=5432 --target-
 
 PGHOST="$(kubectl get svc -n "${PG_NAMESPACE}" "${HOH_PGBOUNCER}" -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')"
 echo "postgres secret name: " $PG_CLUSTER_USER_SECRET_NAME
+echo "postgres host: "$PGHOST
 echo "postgres user: " $PGUSER
 echo "postgres password: " $PGPASSWORD
-echo "postgres host: "$PGHOST
 
 # load file from tpl to db hoh
 echo "PGPASSWORD='$PGPASSWORD' PGSSLMODE=require psql -h $PGHOST -U $DB_NAME -d $DB_NAME"
