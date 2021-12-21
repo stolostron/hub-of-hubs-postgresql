a19f8e0128947f7031f26a0564166837f80d5b31
Vadim Eisenberg <vadime@il.ibm.com>
2021-12-21 06:23:20 +0200

    wait for all the secrets to be created

--- pgo/setup.sh
+++ pgo/setup.sh
@@ -6,20 +6,36 @@ kubectl delete -k ./high-availability --ignore-not-found=true 2>/dev/null
 # ensure the pgo operator crd and other stuff is deleted first to start its deployment from scratch
 kubectl delete -k ./install --ignore-not-found=true 2>/dev/null
 
+pg_namespace="hoh-postgres"
+
+postgres_user_secrets=("hoh-pguser-hoh-process-user" "hoh-pguser-postgres" "hoh-pguser-transport-bridge-user")
+
+# ensure all the user secrets are deleted
+for secret in ${postgres_user_secrets[@]}
+do
+    matched=$(kubectl get secret $secret -n $pg_namespace --ignore-not-found=true)
+    while [ ! -z "$matched" ]; do
+	echo "Waiting for secret $secret to be deleted from namespace $pg_namespace"
+	matched=$(kubectl get secret $secret -n $pg_namespace --ignore-not-found=true)
+	sleep 10
+    done
+done
+
 # install the pgo operator to postgres-operator
 kubectl apply -k ./install
 
 # set up hoh database hoh
 kubectl apply -k ./high-availability
 
-pg_namespace="hoh-postgres"
-
-postgres_secret_name="hoh-pguser-postgres"
-
-while [ -z "$matched" ]; do
-    echo "Waiting for ($pg_namespace/$postgres_secret_name) to be created"
-    matched=$(kubectl get secret $postgres_secret_name -n $pg_namespace --ignore-not-found=true)
-    sleep 10
+# ensure all the user secrets are created
+for secret in ${postgres_user_secrets[@]}
+do
+    matched=$(kubectl get secret $secret -n $pg_namespace --ignore-not-found=true)
+    while [ -z "$matched" ]; do
+	echo "Waiting for secret $secret to be created in namespace $pg_namespace"
+	matched=$(kubectl get secret $secret -n $pg_namespace --ignore-not-found=true)
+	sleep 10
+    done
 done
 
 kubectl delete -f ./postgres-job.yaml --ignore-not-found=true
